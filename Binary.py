b2c7a99a8d8dbef232993641086c960a31b90862
cybervicky100 <115596278+cybervicky100@users.noreply.github.com>
2022-10-12 05:25:29 +0530

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,15 @@
+bnum = int(input())
+
+4 days = 0
+
+5 m=1
+
+blen = len(str(bnum))
+
+8 fork in range(blen):
+
+rem = bnum%10
+
+10 dnum=dnum + (rem *m)m=m*2
+
+ bnum = int(bnum/10) print("\nEquivalent Decimal Value="dnum)
